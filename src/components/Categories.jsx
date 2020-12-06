616fb38ebbc06185b05e77b0f4eb7075197359e0
stepGT <stepgt@mail.ru>
2020-12-07 00:59:16 +1000

    Transfer useSelector to Home component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,38 +1,5 @@
 import React from 'react';
 
-// class Categories extends React.Component {
-//   state = {
-//     activeItem: 0
-//   };
-
-//   onSelectItem = index => {
-//     this.setState({
-//       activeItem: index
-//     });
-//   }
-
-//   render() {
-//     const {items} = this.props;
-//     return (
-//       <div>
-//           <div className="categories">
-//             <ul>
-//               <li>Все</li>
-//               {items.map((name, index) => 
-//                 <li
-//                 className={this.state.activeItem === index ? 'active' : ''}
-//                 onClick={()=>{this.onSelectItem(index)}} 
-//                 key={`${name}__${index}`}>
-//                   {name}
-//                 </li>
-//               )}
-//             </ul>
-//           </div>
-//       </div>
-//     )
-//   };
-// }
-
 function Categories({items}) {
     const [activeItem, setActiveItem] = React.useState(null);
     const onSelectItem = index => {
