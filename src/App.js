20310d04e012df49f8bdc9744c7689144e55ce41
stepGT <stepgt@mail.ru>
2020-12-12 23:46:13 +1000

    Added json-server

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import { setPizzas } from './redux/actions/pizzas';
 function App() {
   const dispatch = useDispatch();
   React.useEffect(() => {
-    axios.get('http://localhost:3000/db.json').then(({ data }) => {
-      dispatch(setPizzas(data.pizzas));
+    axios.get('http://localhost:3001/pizzas').then(({ data }) => {
+      dispatch(setPizzas(data));
     });
   }, []);
   return (
