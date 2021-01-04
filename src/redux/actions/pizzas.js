cb49a583c705cbb1600417b83410699b9e913c12
stepGT <stepgt@mail.ru>
2021-01-05 01:23:13 +1000

    Added Proxying API Requests

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const fetchPizzas = (sortBy, category) => (dispatch) => {
     dispatch(setLoaded(false));
     axios.get(
-        `http://localhost:3001/pizzas?${
+        `/pizzas?${
             category !== null ? `category=${category}` : ''
         }&_sort=${sortBy.type}&_order=${sortBy.order}`)
         .then(({ data }) => {
